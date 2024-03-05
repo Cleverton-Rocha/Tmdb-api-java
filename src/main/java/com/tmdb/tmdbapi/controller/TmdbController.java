bf8dd4573498ed9b2c2cddc477ef9a0bd66a4b1d
ultr4 <cl.rch09@icloud.com>
2024-03-04 23:31:10 -0300

    :bulb: docs: Add swagger annotations

--- src/main/java/com/tmdb/tmdbapi/controller/TmdbController.java
+++ src/main/java/com/tmdb/tmdbapi/controller/TmdbController.java
@@ -27,59 +27,59 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 @SecurityRequirement(name = "bearerAuth")
 @SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
 public class TmdbController {
-  @Autowired
-  private TmdbService tmdbService;
+    @Autowired
+    private TmdbService tmdbService;
 
-  @Operation(description = "Retorna uma lista de filmes bem avaliados.")
-  @ApiResponse(responseCode = "200", description = "Retorna uma lista de filmes.")
-  @GetMapping("/top_rated")
-  public ResponseEntity<MoviePageResponse> getTopRatedMovies(
-      @RequestHeader("Authorization") String bearerToken,
-      @RequestParam(defaultValue = "1") Integer page,
-      @RequestParam(defaultValue = "pt-BR") String language) {
-    return ResponseEntity
-        .ok(tmdbService.getTopRatedMovies(bearerToken, page, language));
-  }
+    @Operation(description = "Retorna uma lista de filmes bem avaliados.")
+    @ApiResponse(responseCode = "200", description = "Retorna uma lista de filmes.")
+    @GetMapping("/top_rated")
+    public ResponseEntity<MoviePageResponse> getTopRatedMovies(
+            @RequestHeader("Authorization") String bearerToken,
+            @RequestParam(defaultValue = "1") Integer page,
+            @RequestParam(defaultValue = "pt-BR") String language) {
+        return ResponseEntity
+                .ok(tmdbService.getTopRatedMovies(bearerToken, page, language));
+    }
 
-  @Operation(description = "Retorna uma lista de filmes procurado pela palavra chave.")
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Retorna uma lista de filmes filmes procurado pela palavra chave."),
-      @ApiResponse(responseCode = "404", description = "Retorna 'Movie not found' caso não ache nenhum filme com a palavra chave indicada.", content = @Content(mediaType = "text/plain", examples = @ExampleObject(value = "Movie not found."))) })
-  @GetMapping("/movie/{movieName}")
-  public ResponseEntity<MoviePageResponse> getMovieByName(
-      @RequestHeader("Authorization") String bearerToken,
-      @PathVariable String movieName,
-      @RequestParam(defaultValue = "1") Integer page,
-      @RequestParam(defaultValue = "pt-BR") String language) {
-    return ResponseEntity
-        .ok(tmdbService.getMovieByName(bearerToken, movieName, page, language));
-  }
+    @Operation(description = "Retorna uma lista de filmes procurado pela palavra chave.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna uma lista de filmes filmes procurado pela palavra chave."),
+            @ApiResponse(responseCode = "404", description = "Retorna 'Movie not found' caso não ache nenhum filme com a palavra chave indicada.", content = @Content(mediaType = "text/plain", examples = @ExampleObject(value = "Movie not found."))) })
+    @GetMapping("/movie/{movieName}")
+    public ResponseEntity<MoviePageResponse> getMovieByName(
+            @RequestHeader("Authorization") String bearerToken,
+            @PathVariable String movieName,
+            @RequestParam(defaultValue = "1") Integer page,
+            @RequestParam(defaultValue = "pt-BR") String language) {
+        return ResponseEntity.ok(tmdbService.getMovieByName(bearerToken,
+                movieName, page, language));
+    }
 
-  @Operation(description = "Retorna uma lista de filmes procurado pela palavra chave do id.")
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Retorna uma lista de filmes procurado pela palavra chave do id."),
-      @ApiResponse(responseCode = "404", description = "Retorna 'Genre not found' caso não ache nenhum gênero com a palavra chave indicada.", content = @Content(mediaType = "text/plain", examples = @ExampleObject(value = "Genre not found."))) })
-  @GetMapping("/genre/{genreName}")
-  public ResponseEntity<MoviePageResponse> getMovieByGenre(
-      @RequestHeader("Authorization") String bearerToken,
-      @PathVariable String genreName,
-      @RequestParam(defaultValue = "1") Integer page,
-      @RequestParam(defaultValue = "pt-BR") String language) {
-    return ResponseEntity.ok(
-        tmdbService.getMoviesByGenre(bearerToken, genreName, page, language));
-  }
+    @Operation(description = "Retorna uma lista de filmes procurado pela palavra chave do id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna uma lista de filmes procurado pela palavra chave do id."),
+            @ApiResponse(responseCode = "404", description = "Retorna 'Genre not found' caso não ache nenhum gênero com a palavra chave indicada.", content = @Content(mediaType = "text/plain", examples = @ExampleObject(value = "Genre not found."))) })
+    @GetMapping("/genre/{genreName}")
+    public ResponseEntity<MoviePageResponse> getMovieByGenre(
+            @RequestHeader("Authorization") String bearerToken,
+            @PathVariable String genreName,
+            @RequestParam(defaultValue = "1") Integer page,
+            @RequestParam(defaultValue = "pt-BR") String language) {
+        return ResponseEntity.ok(tmdbService.getMoviesByGenre(bearerToken,
+                genreName, page, language));
+    }
 
-  @Operation(description = "Retorna um filme procurado pelo id.")
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Retorna um filme procurado pelo id."),
-      @ApiResponse(responseCode = "404", description = "Retorna 'Movie not found' caso não ache nenhum filme com a palavra chave indicada.", content = @Content(mediaType = "text/plain", examples = @ExampleObject(value = "Movie not found."))) })
-  @GetMapping("/movie/{movieId}/details")
-  public ResponseEntity<MovieDetailed> getMovieDetails(
-      @RequestHeader("Authorization") String bearerToken,
-      @PathVariable Integer movieId,
-      @RequestParam(defaultValue = "1") Integer page,
-      @RequestParam(defaultValue = "pt-BR") String language) {
-    return ResponseEntity
-        .ok(tmdbService.getMovieDetails(bearerToken, movieId, page, language));
-  }
+    @Operation(description = "Retorna um filme procurado pelo id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna um filme procurado pelo id."),
+            @ApiResponse(responseCode = "404", description = "Retorna 'Movie not found' caso não ache nenhum filme com a palavra chave indicada.", content = @Content(mediaType = "text/plain", examples = @ExampleObject(value = "Movie not found."))) })
+    @GetMapping("/movie/{movieId}/details")
+    public ResponseEntity<MovieDetailed> getMovieDetails(
+            @RequestHeader("Authorization") String bearerToken,
+            @PathVariable Integer movieId,
+            @RequestParam(defaultValue = "1") Integer page,
+            @RequestParam(defaultValue = "pt-BR") String language) {
+        return ResponseEntity.ok(tmdbService.getMovieDetails(bearerToken,
+                movieId, page, language));
+    }
 }
