26785bfe89e53491b91d5813564aed973db00e60
ultr4 <cl.rch09@icloud.com>
2024-03-04 23:29:49 -0300

    :wrench: chore: Add swagger default URL config

--- src/main/java/com/tmdb/tmdbapi/TmdbapiApplication.java
+++ src/main/java/com/tmdb/tmdbapi/TmdbapiApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {
+		@Server(url = "/", description = "Default Server URL") })
+
 @SpringBootApplication
 @EnableFeignClients
 public class TmdbapiApplication {
